e0b3ae8a2c39ae8bfca5368a6616b5da72401a9b
timmy12335 <63812491+timmy12335@users.noreply.github.com>
2020-11-01 16:48:12 +0800

    msg & 刪除CR內的frank

---
+++ src/main/java/CR/model/CrMsgBean.java
@@ -0,0 +1,309 @@
+package CR.model;
+
+import java.io.Serializable;
+import java.sql.Timestamp;
+
+import javax.persistence.CascadeType;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.Table;
+import javax.persistence.Transient;
+
+import iiiNews.MB.model.CpMemberBean;
+import iiiNews.MB.model.MBBean;
+
+
+@Entity
+@Table(name="CR_msg")
+public class CrMsgBean implements Serializable{
+
+	private static final long serialVersionUID = 1L;
+	
+	@Id
+	@GeneratedValue(strategy=GenerationType.IDENTITY)
+	private Integer pk;
+	
+	private String receivemsg;
+	@Transient
+	private String receiver;
+	private Timestamp receiveTime;
+	private String sendmsg;
+	@Transient
+	private String sender;
+	private Timestamp sendTime;
+	@Transient
+	private Integer memberId;
+	@Transient
+	private Integer companyId;
+	@ManyToOne(cascade=CascadeType.ALL)
+	@JoinColumn(name="member_Id")
+	private MBBean mbBean;
+	@ManyToOne(cascade=CascadeType.ALL)
+	@JoinColumn(name="company_Id")
+	private CpMemberBean cpBean;
+	
+	@Override
+	public String toString() {
+		return "CrMsgBean [pk=" + pk + ", receivemsg=" + receivemsg + ", receiver=" + receiver + ", receiveTime="
+				+ receiveTime + ", sendmsg=" + sendmsg + ", sender=" + sender + ", sendTime=" + sendTime + ", memberId="
+				+ memberId + ", companyId=" + companyId + ", mbBean=" + mbBean + ", cpBean=" + cpBean + "]";
+	}
+
+
+
+
+
+
+
+	public CrMsgBean(Integer pk, String receivemsg, String receiver, Timestamp receiveTime, String sendmsg,
+			String sender, Timestamp sendTime, Integer memberId, Integer companyId, MBBean mbBean,
+			CpMemberBean cpBean) {
+		super();
+		this.pk = pk;
+		this.receivemsg = receivemsg;
+		this.receiver = receiver;
+		this.receiveTime = receiveTime;
+		this.sendmsg = sendmsg;
+		this.sender = sender;
+		this.sendTime = sendTime;
+		this.memberId = memberId;
+		this.companyId = companyId;
+		this.mbBean = mbBean;
+		this.cpBean = cpBean;
+	}
+
+
+
+
+
+
+
+	public Integer getPk() {
+		return pk;
+	}
+
+
+
+
+
+
+
+	public void setPk(Integer pk) {
+		this.pk = pk;
+	}
+
+
+
+
+
+
+
+	public String getReceivemsg() {
+		return receivemsg;
+	}
+
+
+
+
+
+
+
+	public void setReceivemsg(String receivemsg) {
+		this.receivemsg = receivemsg;
+	}
+
+
+
+
+
+
+
+	public String getReceiver() {
+		return receiver;
+	}
+
+
+
+
+
+
+
+	public void setReceiver(String receiver) {
+		this.receiver = receiver;
+	}
+
+
+
+
+
+
+
+	public Timestamp getReceiveTime() {
+		return receiveTime;
+	}
+
+
+
+
+
+
+
+	public void setReceiveTime(Timestamp receiveTime) {
+		this.receiveTime = receiveTime;
+	}
+
+
+
+
+
+
+
+	public String getSendmsg() {
+		return sendmsg;
+	}
+
+
+
+
+
+
+
+	public void setSendmsg(String sendmsg) {
+		this.sendmsg = sendmsg;
+	}
+
+
+
+
+
+
+
+	public String getSender() {
+		return sender;
+	}
+
+
+
+
+
+
+
+	public void setSender(String sender) {
+		this.sender = sender;
+	}
+
+
+
+
+
+
+
+	public Timestamp getSendTime() {
+		return sendTime;
+	}
+
+
+
+
+
+
+
+	public void setSendTime(Timestamp sendTime) {
+		this.sendTime = sendTime;
+	}
+
+
+
+
+
+
+
+	public Integer getMemberId() {
+		return memberId;
+	}
+
+
+
+
+
+
+
+	public void setMemberId(Integer memberId) {
+		this.memberId = memberId;
+	}
+
+
+
+
+
+
+
+	public Integer getCompanyId() {
+		return companyId;
+	}
+
+
+
+
+
+
+
+	public void setCompanyId(Integer companyId) {
+		this.companyId = companyId;
+	}
+
+
+
+
+
+
+
+	public MBBean getMbBean() {
+		return mbBean;
+	}
+
+
+
+
+
+
+
+	public void setMbBean(MBBean mbBean) {
+		this.mbBean = mbBean;
+	}
+
+
+
+
+
+
+
+	public CpMemberBean getCpBean() {
+		return cpBean;
+	}
+
+
+
+
+
+
+
+	public void setCpBean(CpMemberBean cpBean) {
+		this.cpBean = cpBean;
+	}
+
+
+
+
+
+
+
+	public CrMsgBean() {
+		
+	}
+
+}

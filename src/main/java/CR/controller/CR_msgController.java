9e36ca369a78d79fcfb2cde692f27981d168726f
timmy12335 <63812491+timmy12335@users.noreply.github.com>
2020-11-02 11:21:55 +0800

    註解

--- src/main/java/CR/controller/CR_msgController.java
+++ src/main/java/CR/controller/CR_msgController.java
@@ -1,19 +1,19 @@
-package CR.controller;
-
-import javax.servlet.ServletContext;
-
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.RestController;
-
-import CR.service.CR_msgService;
-
-@RestController
-public class CR_msgController {
-
-	@Autowired
-	ServletContext ctx;
-	
-	@Autowired
-	CR_msgService service;
-	
-}
+//package CR.controller;
+//
+//import javax.servlet.ServletContext;
+//
+//import org.springframework.beans.factory.annotation.Autowired;
+//import org.springframework.web.bind.annotation.RestController;
+//
+//import CR.service.CR_msgService;
+//
+//@RestController
+//public class CR_msgController {
+//
+//	@Autowired
+//	ServletContext ctx;
+//	
+//	@Autowired
+//	CR_msgService service;
+//	
+//}

67206c15880ffecfeacf3f032899194e5eb5343c
timmy12335 <63812491+timmy12335@users.noreply.github.com>
2020-11-22 11:07:55 +0800

    員工判斷

--- src/main/java/CR/validation/CRempValidator.java
+++ src/main/java/CR/validation/CRempValidator.java
@@ -21,14 +21,14 @@ public class CRempValidator implements Validator {
 		ValidationUtils.rejectIfEmptyOrWhitespace(errors, "empId", "", "員工帳號不得為空");
 		ValidationUtils.rejectIfEmptyOrWhitespace(errors, "empemail", "", "員工信箱不得為空");
 
-		if(empbean.getEmpName() != null && empbean.getEmpName() .length() <=0) {
+		if(empbean.getEmpName() != null && empbean.getEmpName() .length() <= -1) {
 			errors.rejectValue("empName", "","輸入有誤，請重新輸入");
 		}
-		if(empbean.getEmpId() != null && empbean.getEmpId().length() <=0) {
+		if(empbean.getEmpId() != null && empbean.getEmpId().length() <= -1) {
 			errors.rejectValue("empId", "","輸入有誤，請重新輸入");
 		}
 		String format = "\\p{Alpha}\\w{2,15}[@][a-z0-9]{3,}[.]\\p{Lower}{2,}";
-		if(empbean.getEmpemail() != null && empbean.getEmpemail().length() <=0) {
+		if(empbean.getEmpemail() != null && empbean.getEmpemail().length() <= -1) {
 			errors.rejectValue("empemail", "","輸入有誤，請重新輸入");
 		}
 		if(empbean.getEmpemail() != null && empbean.getEmpemail().matches(format)) {
